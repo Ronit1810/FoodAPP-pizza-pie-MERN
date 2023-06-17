47173293f999eb87c8981fc0afb1136a7eff1770
RONIT PATEL <ronitpatel1810@gmail.com>
2023-06-17 14:21:28 +0530

    complete home- banner page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Footer from './component/Footer/Footer';
 import Header from '../src/component/Header/Header';
+import Banner from './screen/Banner/Banner';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Banner />
       <Routes>
         {/* <Route path='/' element={<Home />} /> */}
       </Routes>
---
+++ src/screen/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import "./Banner.scss"
+import ban from "../../assets/pizza delivery.jpeg"
+
+const Banner = () =>{
+    return(
+        <div className="hero-banner">
+            <div className="banner-content">
+                <div className="text-content">
+                    <div className="span-1">Quality Food</div>
+                    <h1>Fastest Delivery & Easy Pickup</h1>
+                    <span className="span-2">Best cooks and best dilevery guys all at your service. Hot tasty food will reach you in 60 minutes.</span>
+                    <div className="form">
+                        <input type="text" autoFocus placeholder="Food Search" />
+                        <div className="text">Search</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={ban} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
